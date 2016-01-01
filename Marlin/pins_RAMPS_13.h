563f71af4ba65f8b7403b4b3e7abe28818791fb7
hsushimooshi <hsushimooshi@gmail.com>
2016-01-01 11:11:01 -0800

    Update pins_RAMPS_13.h
    configured to CoreXY printer

--- Marlin/pins_RAMPS_13.h
+++ Marlin/pins_RAMPS_13.h
@@ -33,6 +33,7 @@
 #else
   #define SERVO0_PIN        7 // RAMPS_13 // Will conflict with BTN_EN2 on LCD_I2C_VIKI
 #endif
+#define CONTROLLERFAN_PIN  16
 #define SERVO1_PIN          6
 #define SERVO2_PIN          5
 #define SERVO3_PIN          4
@@ -40,19 +41,19 @@
 #define X_STEP_PIN         54
 #define X_DIR_PIN          55
 #define X_ENABLE_PIN       38
-#define X_MIN_PIN           3
+#define X_MIN_PIN          -1//3
 #define X_MAX_PIN           2
 
 #define Y_STEP_PIN         60
 #define Y_DIR_PIN          61
 #define Y_ENABLE_PIN       56
-#define Y_MIN_PIN          14
+#define Y_MIN_PIN          -1//14
 #define Y_MAX_PIN          15
 
 #define Z_STEP_PIN         46
 #define Z_DIR_PIN          48
 #define Z_ENABLE_PIN       62
-#define Z_MIN_PIN          18
+#define Z_MIN_PIN          -1//18
 #define Z_MAX_PIN          19
 
 #define E0_STEP_PIN        26
